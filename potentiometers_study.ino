2e111e2ac232e10237b96e106bb8abe3df92f49c
JohannaHim78 <himjohanna78@gmail.com>
2025-07-15 13:39:15 -0400

    Update potentiometers_study.ino

--- potentiometers_study.ino
+++ potentiometers_study.ino
@@ -7,15 +7,13 @@ int timeDelay = 1000;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(outputPin, OUTPUT);
 }
 
 void loop() {
-
-  digitalWrite(outputPin, HIGH);
-
   vRaw = analogRead(vPin);
   realVout = (5./1023.) * vRaw; 
   Serial.println(realVout);
+
+  //prints new voltage every second 
   delay(timeDelay);
 }
